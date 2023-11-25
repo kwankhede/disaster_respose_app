933644c4b90c3b1dd78bc80aabb0b39106e49ed1
Kapil Wankhede <kapilwankhede22@gmail.com>
2023-11-25 18:14:38 +0530

    Add files via upload

---
+++ models/train_classifier.py
@@ -0,0 +1,155 @@
+import sys
+import re
+import pickle
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.metrics import classification_report
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import make_scorer, accuracy_score, f1_score, fbeta_score
+from scipy.stats import hmean
+from scipy.stats.mstats import gmean
+import joblib
+
+nltk.download(["punkt", "wordnet", "stopwords"])
+nltk.download(["averaged_perceptron_tagger"])
+
+
+def load_data(database_filepath):
+    engine = create_engine("sqlite:///" + database_filepath)
+    df = pd.read_sql_table("Disaster_Response", engine)
+    X = df["message"]
+    Y = df.drop(["id", "message", "original", "genre"], axis=1)
+    category_names = Y.columns.tolist()
+    return X, Y, category_names
+
+
+# Change the name of the tokenize function
+def custom_tokenize(text):
+    text.lower()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word not in stopwords.words("english")]
+    tokens = [WordNetLemmatizer().lemmatize(word).strip() for word in tokens]
+    return tokens
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(custom_tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ["VB", "VBP"] or first_word == "RT":
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+
+def model_pipeline():
+    pipeline_modified = Pipeline(
+        [
+            (
+                "features",
+                FeatureUnion(
+                    [
+                        (
+                            "text_pipeline",
+                            Pipeline(
+                                [
+                                    ("vect", CountVectorizer(tokenizer=tokenize)),
+                                    ("tfidf", TfidfTransformer()),
+                                ]
+                            ),
+                        ),
+                        ("starting_verb", StartingVerbExtractor()),
+                    ]
+                ),
+            ),
+            ("clf", RandomForestClassifier()),
+        ]
+    )
+    return pipeline_modified
+
+
+def build_model():
+    model = model_pipeline()
+    parameters = {
+        "features__text_pipeline__vect__ngram_range": ((1, 1), (1, 2)),
+        "features__text_pipeline__vect__max_df": (0.5, 0.75, 1.0),
+        "features__text_pipeline__vect__max_features": (None, 5000, 10000),
+        "features__text_pipeline__tfidf__use_idf": (True, False),
+        "clf__n_estimators": [50, 100, 200],
+        "clf__min_samples_split": [2, 3, 4],
+        "features__transformer_weights": (
+            {"text_pipeline": 1, "starting_verb": 0.5},
+            {"text_pipeline": 0.5, "starting_verb": 1},
+            {"text_pipeline": 0.8, "starting_verb": 1},
+        ),
+    }
+    cv = GridSearchCV(model, param_grid=parameters)
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    predict_y = model.predict(X_test)
+    for i in range(len(category_names)):
+        category = category_names[i]
+        print(category)
+        print(classification_report(Y_test[category], predict_y[:, i]))
+
+
+def save_model(model, model_filepath):
+    with open(model_filepath, "wb") as file:
+        pickle.dump(model, file)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print("Loading data...\n    DATABASE: {}".format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+        print("Building model...")
+        model = build_model()
+
+        print("Training model...")
+        model.fit(X_train, Y_train)
+
+        print("Evaluating model...")
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print("Saving model...\n    MODEL: {}".format(model_filepath))
+        save_model(model, model_filepath)
+
+        print("Trained model saved!")
+
+    else:
+        print(
+            "Please provide the filepath of the disaster messages database "
+            "as the first argument and the filepath of the pickle file to "
+            "save the model to as the second argument. \n\nExample: python "
+            "train_classifier.py ../data/DisasterResponse.db classifier.pkl"
+        )
+
+
+if __name__ == "__main__":
+    main()
