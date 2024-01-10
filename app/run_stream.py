5de2168462c863268cb503095d4050f26d745d1a
Kapil Wankhede <kapilwankhede22@gmail.com>
2024-01-10 20:47:06 +0530

    Update run_stream.py

--- app/run_stream.py
+++ app/run_stream.py
@@ -80,7 +80,8 @@ def main():
         # Get user input with a box around it
         st.markdown("<br><br> <br>", unsafe_allow_html=True)  # Adding space
         # Custom styling for the label
-        st.markdown("<span style='color:red; font-size:40px; font-weight:bold;'>ENTER A MESSAGE BELOW :</span>", unsafe_allow_html=True)
+        st.markdown("<span style='color:red; font-size:40px; font-weight:bold; text-decoration:underline;'>ENTER A MESSAGE BELOW :</span>", unsafe_allow_html=True)
+
 
 
         # Get user input with a box around it
